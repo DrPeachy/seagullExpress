8445d5e88fed765b57c859e1750706a28cdc8997
DrPeachy <p1067838263@gmail.com>
2022-04-15 11:05:51 -0400

    camera

---
+++ Assets/Scripts/SmoothCam.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCam : MonoBehaviour
+{
+	public GameObject target;
+	private Vector3 targetPos;
+	public float moveSpd;
+	
+	void Update () 
+	{
+		targetPos = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+		Vector3 velocity = targetPos - transform.position;
+		transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 1.0f, moveSpd * Time.deltaTime);	
+	}
+}
