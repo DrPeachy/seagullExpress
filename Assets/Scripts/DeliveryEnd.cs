c0037dffafe615af36d1a3a8dbdcc3cc32f191c6
patrick-cy-106 <pc2720@nyu.edu>
2022-04-21 17:50:26 -0400

    mission end scene

--- Assets/Scripts/DeliveryEnd.cs
+++ Assets/Scripts/DeliveryEnd.cs
@@ -7,14 +7,23 @@ public class DeliveryEnd : MonoBehaviour
 {
     public TextMeshProUGUI resultsUI;
     string results = "";
+    int totalPay = 0;
 
     void Start()
-    {
+    {   
+        int pkgPickedUp = 0;
         foreach (package p in PubVar.packages) {
             if (p.GetState() == "Delivered" || p.GetState() == "Broken") {
-                results += p.ResultString() + "\n";
+                pkgPickedUp += 1;
+                totalPay += p.Results(ref results);
+                results += "\n";
             }
+            results += "\n";
         }
+        if (pkgPickedUp == 0) {
+            results = "No Packages Picked Up\n";
+        }
+        results += " \nTotal Pay: $" + totalPay;
     }
 
     void Update()
