750c558a160e806a65618c76b4cdbc6aef71bfac
DrPeachy <p1067838263@gmail.com>
2022-05-05 11:06:54 -0400

    fix bug

--- Assets/Scripts/obstacles/create_objects.cs
+++ Assets/Scripts/obstacles/create_objects.cs
@@ -23,21 +23,24 @@ public class create_objects : MonoBehaviour
         //
         _obj_rd = objects_to_create.GetComponent<Renderer>();
         maincam = Camera.main;
-        //StartCoroutine(create());
+        StartCoroutine(create());
+
     }
 
     private void Update() {
         // can add conditions to stop creating by change keep_creating
-        while(j < objLimit){
-            create_objects_once();
-            j++;
-        }
+        // while(j < objLimit){
+        //     create_objects_once();
+        //     j++;
+        // }
+        
     }
 
     IEnumerator create(){
         while(keep_creating){
             i = 0;
             if(j < objLimit) create_objects_once();
+            print("create");
             yield return new WaitForSeconds(cool_down);
         }
     }
@@ -45,26 +48,29 @@ public class create_objects : MonoBehaviour
     void create_objects_once(){
         while(i < nums_to_create_each_time){
             // 20 and 10 are the camera size, which are constant
-            x = Random.Range(maincam.transform.position.x - 20, maincam.transform.position.x + 20);
-            y = Random.Range(maincam.transform.position.y - 15, maincam.transform.position.y + 15);
+            x = Random.Range(maincam.transform.position.x - 30, maincam.transform.position.x + 30);
+            y = Random.Range(maincam.transform.position.y - 20, maincam.transform.position.y + 20);
             Vector2 pos = new Vector2(x,y);
             Vector2 temp = maincam.WorldToViewportPoint(pos);
-            if(Physics2D.BoxCast(pos, _obj_rd.bounds.size*2, 0, Vector2.up, .01f) ||
-                (temp.x>=-.2f && temp.x<=1.2f && temp.y>=-0.2f && temp.y<=1.2f)){
+            if(temp.x>=-.2f && temp.x<=1.2f && temp.y>=-0.2f && temp.y<=1.2f){
+                print("cannot instantiate");
                 continue;
             } else {
                 GameObject newObj = Instantiate(objects_to_create, pos, Quaternion.identity);
-                //StartCoroutine(deleteAfter(x, y, newObj));
+                print($"obj: {i}");
+                i++;
+                j++;
+                StartCoroutine(deleteAfter(newObj));
             }
         }
     }
 
 
-    IEnumerator deleteAfter(float x, float y, GameObject obj){
+    IEnumerator deleteAfter(GameObject obj){
         bool flag = true;
         while(flag){
             yield return new WaitForSeconds(cool_down);
-            if(Vector3.Distance(maincam.transform.position, obj.transform.position) > 25){
+            if(Vector2.Distance(maincam.transform.position, obj.transform.position) > 20){
                     Destroy(obj);
                     j--;
                     flag = false;
