a28c72cc4ae0c56e1d29b037286a5397eb432490
reginauwu <wregina0418@gmail.com>
2022-05-06 13:50:26 -0400

    sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -93,7 +93,6 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Obstacle")){
-            // soundManagerScript.playSound("hitCloud");
             soundManagerScript.playSound("boing");
             StartCoroutine(WaitTillRestore());
         }
